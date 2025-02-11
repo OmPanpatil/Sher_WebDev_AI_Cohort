89186ecc7a182cc3f34cb69b8324bfb5e93e8c19
Om Panpatil <ompanpatil513@gmail.com>
2025-02-11 22:03:45 +0530

    Sher Learning

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -2,4 +2,4 @@ var dt = new Date();
 // console.log(dt.getHours()%12);
 // console.log(dt.getMinutes()%12);
 
-var str = dt.getHours()%12 + ":" + dt
+var str = dt.getHours()%12 + ":" + dt.getMinutes()%12;
